4ce97cb4315d26dab33d7f09ad946ce194c2cc1d
gituser <test@test.com>
2018-01-25 19:08:02 -0500

    asdfasd

--- TestProj/src/com/test/Test.java
+++ TestProj/src/com/test/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Testing..changed branching5");
+		System.out.println("Testing..changed branching8");
 	}
 
 }
