93776b98522a0aaad28594e470ac15149d98e75d
gituser <test@test.com>
2018-01-25 22:48:58 -0500

    conflict test from master

--- TestProj/src/com/test/Test.java
+++ TestProj/src/com/test/Test.java
@@ -5,7 +5,6 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Conflicts testing changes done in master");
-		System.out.println("Testing..changed branching. Release changes");
 	}
 
 }
